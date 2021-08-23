8d02b270f0e4353c9811d6acf975aaff4174ff41
Sunmita <79628610+Sunmita@users.noreply.github.com>
2021-08-23 21:38:22 +0530

    Update python code.py

--- python code.py
+++ python code.py
@@ -1,54 +1,53 @@
-
 import socketio
-import eventlet
+event submission
 import numpy as np
-from flask import Flask
-from keras.models import load_model
-import base64
+from Flask import Flask
+from keras.models to import load_model
+enter base64
 from io import BytesIO
-from PIL import Image
+from PIL image import
 import cv2
 
-sio = socketio.Server()
+si = = socketio.Server ()
 
-app = Flask(__name__) #'__main__'
+app = Flask (__ name__) # '__ main__'
 speed_limit = 10
-def img_preprocess(img):
-    img = img[60:135,:,:]
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
-    img = cv2.GaussianBlur(img,  (3, 3), 0)
-    img = cv2.resize(img, (200, 66))
-    img = img/255
+def img_preprocess (img):
+    img = img [60: 135,:,:]
+    img = cv2.cvtColor (img, cv2.COLOR_RGB2YUV)
+    img = cv2.GaussianBlur (img, (3, 3), 0)
+    img = cv2.resize (img, (200, 66))
+    img = img / 255
     return img
 
 
-@sio.on('telemetry')
-def telemetry(sid, data):
-    speed = float(data['speed'])
-    image = Image.open(BytesIO(base64.b64decode(data['image'])))
-    image = np.asarray(image)
-    image = img_preprocess(image)
-    image = np.array([image])
-    steering_angle = float(model.predict(image))
-    throttle = 1.0 - speed/speed_limit
-    print('{} {} {}'.format(steering_angle, throttle, speed))
-    send_control(steering_angle, throttle)
+@ sio.on ('telemetry')
+def telemetry (sid, data):
+    speed = float (data ['speed'])
+    image = Image.open (BytesIO (base64.b64decode (data ['image'])))
+    image = np.asarray (photo)
+    image = img_preprocess (image)
+    image = np.array ([photo])
+    steering_angle = float (model. prediction (image))
+    pinch = 1.0 - speed / speed_limit
+    print ('{} {}'. format (steering_angle, pinch, speed))
+    send_control (steering_angle, pinch)
 
 
 
-@sio.on('connect')
-def connect(sid, environ):
-    print('Connected')
-    send_control(0, 0)
+@ sio.on ('connect')
+def connect (sid, environ):
+    Print ('Connected')
+    send_control (0, 0)
 
-def send_control(steering_angle, throttle):
-    sio.emit('steer', data = {
-        'steering_angle': steering_angle.__str__(),
-        'throttle': throttle.__str__()
+def send_control (steering_angle, pinch):
+    sii.emit ('steer', data = {
+        `
+        'pinch': pinch .__ str __ ()
     })
 
 
 if __name__ == '__main__':
-    model = load_model('model.h5')
-    app = socketio.Middleware(sio, app)
-    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
+    model = load_model ('model.h5')
+    application = socketio.Middleware (not, application)
+    eventlet.wsgi.server (eventlet.listen (('', 4997)), app)
